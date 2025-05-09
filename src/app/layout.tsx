12e9d96eb9cff7e4bd53a7b23dec1fd9710292c4
sherzod38 <nadirovsherzod82@gmil.com>
2025-05-09 19:43:45 +0500

    a4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/ui/navbar";  // to‘g‘ri
+import Navbar from "@/components/ui/navbar";  // to&apos;g&apos;ri
 
 
 const geistSans = Geist({
@@ -15,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Kitoblar do'koni",
-  description: "Next.js va Supabase asosida kitoblar do'koni",
+  title: "Kitoblar",
+  description: "Next.js va Supabase asosida kitoblarni qo&apos;shish",
 };
 
 export default function RootLayout({
