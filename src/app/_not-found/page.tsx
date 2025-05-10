2d8b922aed77706cfd7c35d23a4a755455288c6d
sherzod38 <nadirovsherzod82@gmil.com>
2025-05-10 08:42:36 +0500

    s7

--- src/app/_not-found/page.tsx
+++ src/app/_not-found/page.tsx
@@ -1,12 +1,14 @@
 
+import Link from "next/link";
+
 export default function NotFoundPage() {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen bg-gray-50">
       <h1 className="text-4xl font-bold text-red-600">404 - Sahifa topilmadi</h1>
       <p className="mt-4 text-lg text-gray-700">Kechirasiz, siz izlagan sahifa mavjud emas.</p>
-      <link href="/" className="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+      <Link href="/" className="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
         Bosh sahifaga qaytish
-      </link>
+      </Link>
     </main>
   );
 }
