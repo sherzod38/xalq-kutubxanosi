411924d2303ad2d84c93bc4d5383907e0ceffc92
sherzod38 <nadirovsherzod82@gmil.com>
2025-05-19 08:30:36 +0500

    z4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,11 @@ import { LogOut } from 'lucide-react';
 import SearchForm from './SearchForm';
 import { useState, useEffect } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { User } from '@supabase/supabase-js';
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<User | null>(null);
   const supabase = createClientComponentClient();
 
   useEffect(() => {
