390c2131dcd54fa2b2f911b02fac4d81078acae2
sherzod38 <nadirovsherzod82@gmil.com>
2025-05-20 07:06:26 +0500

    z35

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function RegisterPage() {
-  const router = useRouter();
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
